daca48e769afc33cc88c05c25f6db53e0a548a92
Alexander Selzer <aselzer3@gmail.com>
2017-04-21 13:05:29 +0200

    Version update to 0.2.4 and improved testing slightly

--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
 var pcap = require("pcap")
 var radioparse = require("../index")
+var fs = require("fs");
+var crypto = require('crypto');
 
 var session = pcap.createSession("mon0")
 
 session.on("packet", function(raw) {
-  var packet = radioparse.parse(radioparse.slice_packet(raw))
+  try {
+    var packet = radioparse.parse(radioparse.slice_packet(raw))
+  } catch (e) {
+    fs.writeFile("bad_packets/" + crypto.createHash('sha256').digest(raw), radioparse.slice_packet(raw));
+  }
 
   /*if (packet.frame) {
     if (packet.frame.type === 0 && packet.frame.subtype === 4) 
